a7a773711402f47fb603900297da676dcba56eb6
Divyansh Arya <divyansharya@Divyanshs-MacBook-Air.local>
2026-03-29 16:14:35 +0530

    feat: integrate Redis for persistent job storage

--- backend/app/services/job_store.py
+++ backend/app/services/job_store.py
@@ -1,20 +1,34 @@
 
 import uuid
+import json
+from app.services.redis_service import redis_client
 
-jobs = {}
 
 def create_job():
     job_id = str(uuid.uuid4())
-    jobs[job_id] = {
-        "status": "pending",
-        "result": None
-    }
+
+    redis_client.set(
+        job_id,
+        json.dumps({
+            "status": "pending",
+            "result": None
+        })
+    )
     return job_id
 
 def complete_job(job_id, result):
-    if job_id in jobs:
-        jobs[job_id]["status"] = "completed"
-        jobs[job_id]["result"] = result
+    redis_client.set(
+        job_id,
+        json.dumps({
+            "status": "completed",
+            "result": result
+        })
+    )
 
 def get_job(job_id):
-    return jobs.get(job_id)        
+    data = redis_client.get(job_id)
+
+    if not data:
+        return None
+
+    return json.loads(data)
