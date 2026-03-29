0a7aef7abd4f8e6858e71a152c8beac53dd5c885
Divyansh Shukla <divyanshshukla43242@gmail.com>
2026-03-29 16:13:54 +0530

    Attaching Backend and Frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,2 +1,10 @@
 
 // Write your code here // 
+import axios from "axios";
+
+export const analyzeRepo = async (url) => {
+  const res = await axios.post("http://127.0.0.1:8000/analyze-repo", {
+    repo_url: url,
+  });
+  return res.data;
+};
