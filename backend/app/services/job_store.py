6227496638e9c188aaa46876ec97a8e5ba846f86
Divyansh Arya <divyansharya@Divyanshs-MacBook-Air.local>
2026-03-29 18:13:23 +0530

    feat: integrate Celery for async background processing

--- backend/app/services/job_store.py
+++ backend/app/services/job_store.py
@@ -1,6 +1,5 @@
 
 import uuid
-<<<<<<< HEAD
 import json
 from app.services.redis_service import redis_client
 
@@ -15,7 +14,9 @@ def create_job():
             "result": None
         })
     )
+
     return job_id
+
 
 def complete_job(job_id, result):
     redis_client.set(
@@ -26,6 +27,7 @@ def complete_job(job_id, result):
         })
     )
 
+
 def get_job(job_id):
     data = redis_client.get(job_id)
 
@@ -33,23 +35,3 @@ def get_job(job_id):
         return None
 
     return json.loads(data)
-=======
-
-jobs = {}
-
-def create_job():
-    job_id = str(uuid.uuid4())
-    jobs[job_id] = {
-        "status": "pending",
-        "result": None
-    }
-    return job_id
-
-def complete_job(job_id, result):
-    if job_id in jobs:
-        jobs[job_id]["status"] = "completed"
-        jobs[job_id]["result"] = result
-
-def get_job(job_id):
-    return jobs.get(job_id)        
->>>>>>> 20b91bfedf42484b71b9d9ee2e68944395b49316
--- backend/app/tasks/analysis_task.py
+++ backend/app/tasks/analysis_task.py
@@ -1,1 +1,13 @@
-#code here
+#celery worker task to run the analysis in the background and store the result in redis
+
+from app.celery_app import celery
+from app.services.analysis_service import run_analysis
+from app.services.job_store import complete_job
+
+@celery.task
+def run_analysis_task(job_id, owner,repo):
+    try:
+        result = run_analysis(owner, repo)
+        complete_job(job_id, result)
+    except Exception as e:
+        complete_job(job_id, {"error": str(e)})
